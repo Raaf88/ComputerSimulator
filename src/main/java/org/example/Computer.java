7041abdd221a9f577666c6a4a51c04e85eb26715
Raaf88 <rafalewandowski8@gmail.com>
2023-01-29 19:41:23 +0100

    .

--- src/main/java/org/example/Computer.java
+++ src/main/java/org/example/Computer.java
@@ -1,6 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
     private Monitor monitor;
     private Drive drive;
+    private HeadPhones headPhones;
+
+    private List<USBDevice> usbDevices = new ArrayList<>();
+
 }
