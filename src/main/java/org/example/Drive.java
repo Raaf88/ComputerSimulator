03f0259ecd77ace77cf9a0a5d11abcaa15660b16
Raaf88 <rafalewandowski8@gmail.com>
2023-01-29 20:02:55 +0100

    Add Map to Drive class

--- src/main/java/org/example/Drive.java
+++ src/main/java/org/example/Drive.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Drive {
     List<File> hddFiles = new ArrayList<>();
+    Map<String, File> ssdFiles = new HashMap<>();
+
 }
