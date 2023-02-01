c57adad701748fbc536af7b037c1a8e3c50a1985
Raaf88 <rafalewandowski8@gmail.com>
2023-02-01 17:50:06 +0100

    Create File intefrace and Enumm FileType

---
+++ src/main/java/org/example/file/File.java
@@ -0,0 +1,10 @@
+package org.example.file;
+
+public interface File {
+    String getName();
+
+    int getSize();
+
+    FileType getType();
+
+}
---
+++ src/main/java/org/example/file/FileType.java
@@ -0,0 +1,5 @@
+package org.example.file;
+
+public enum FileType {
+    MUSIC,IMAGE
+}
